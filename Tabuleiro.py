6ee183fd3b95d715b05168cdf0c769026675c9a2
lemelusca <l.leme.silva@hotmail.com>
2016-04-19 15:49:18 -0300

    Ajuste do tamanho dos botões

--- Tabuleiro.py
+++ Tabuleiro.py
@@ -13,34 +13,41 @@ class Tabuleiro:
         self.tabuleiro = tk.Tk()
         self.tabuleiro.title("Jogo da Velha")
         
-        self.botao1 = tk.Button(self.tabuleiro)
-        self.botao1.grid(row=0, column=0)
+        self.botao1 = tk.Button(self.tabuleiro, height=6, width= 13)
+        self.botao1.grid(row=0, column=0,)
         
-        self.botao2 = tk.Button(self.tabuleiro)
+        self.botao2 = tk.Button(self.tabuleiro, height=6, width= 13)
         self.botao2.grid(row=0, column=1)
         
-        self.botao3 = tk.Button(self.tabuleiro)
+        self.botao3 = tk.Button(self.tabuleiro, height=6, width= 13)
         self.botao3.grid(row=0, column=2)
         
-        self.botao4 = tk.Button(self.tabuleiro)
+        self.botao4 = tk.Button(self.tabuleiro, height=6, width= 13)
         self.botao4.grid(row=1, column=0)
         
-        self.botao5 = tk.Button(self.tabuleiro)
+        self.botao5 = tk.Button(self.tabuleiro, height=6, width= 13)
         self.botao5.grid(row=1, column=1)
         
-        self.botao6 = tk.Button(self.tabuleiro)
+        self.botao6 = tk.Button(self.tabuleiro, height=6, width= 13)
         self.botao6.grid(row=1, column=2)
         
-        self.botao7 = tk.Button(self.tabuleiro)
+        self.botao7 = tk.Button(self.tabuleiro, height=6, width= 13)
         self.botao7.grid(row=2, column=0)
         
-        self.botao8 = tk.Button(self.tabuleiro)
+        self.botao8 = tk.Button(self.tabuleiro, height=6, width= 13)
         self.botao8.grid(row=2, column=1)
         
-        self.botao9 = tk.Button(self.tabuleiro)
+        self.botao9 = tk.Button(self.tabuleiro, height=6, width= 13)
         self.botao9.grid(row=2, column=2)
         
+        self.jogada = tk.Entry(self.tabuleiro, width= 50)
+        self.jogada.grid(row=4, column=0, columnspan=4)
+        
+        
+        
+        
     def iniciar(self):
+        self.tabuleiro.geometry("300x330")
         self.tabuleiro.mainloop()
         
 jogo=Tabuleiro()
