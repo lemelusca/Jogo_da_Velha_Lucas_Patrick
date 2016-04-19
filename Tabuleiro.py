f52dc639a92f907b89e388f96511ee452810969d
lemelusca <l.leme.silva@hotmail.com>
2016-04-19 13:50:42 -0300

    Criação da calsse Tabuleiro

--- Tabuleiro.py
+++ Tabuleiro.py
@@ -5,3 +5,19 @@ Created on Mon Apr 18 16:53:11 2016
 @author: Lucas
 """
 
+import tkinter as tk
+
+class Tabuleiro:
+    
+    def __init__(self):
+        self.tabuleiro = tk.Tk()
+        self.tabuleiro.title("Jogo da Velha")
+        
+    def iniciar(self):
+        self.tabuleiro.mainloop()
+        
+jogo=Tabuleiro()
+jogo.iniciar() 
+        
+    
+
